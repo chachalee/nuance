3baeb45286c39617449f47a3cea454e8abc62c54
Edith Li <abenoy05@gmail.com>
2016-11-12 14:27:24 -0500

    new

---
+++ configuration.js
@@ -0,0 +1,17 @@
+// Credentials
+
+var URL = 'wss://ws.dev.nuance.com/?';
+
+var APP_ID = "NMDPTRIAL_abenoy05_gmail_com20161111151814";
+var APP_KEY = "9d7d19cd4aefbc3fe11e3cc3e51993ccd5c89061abe68824f31e5071aa425948bf72150a61d14943f08506d59e99a2736232d40d185c310b7b817c25c3d66d82";
+var USER_ID = "";
+var NLU_TAG = "M4259_A2053";
+
+// ASR
+// See: https://developer.nuance.com/public/index.php?task=supportedLanguages
+var ASR_LANGUAGE = "eng-USA";
+
+// TTS
+// See: https://developer.nuance.com/public/index.php?task=supportedLanguages
+var TTS_LANGUAGE = "eng-USA";
+var TTS_VOICE = "";
